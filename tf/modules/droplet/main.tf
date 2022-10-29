22475276d54e12e02166d88cdd409cbe70fdeeaa
PhiltasticGuy <philippe.turcotte.pt@gmail.com>
2022-10-29 18:51:04 +0200

    Moved droplet init bash commands to server-setup.sh file.

--- tf/modules/droplet/main.tf
+++ tf/modules/droplet/main.tf
@@ -21,7 +21,10 @@ resource "digitalocean_droplet" "web" {
   ]
   vpc_uuid = var.vpc_uuid
 
-  tags = []
+  tags = [
+    "${var.project_name}-webserver",
+    "${var.project_name}"
+  ]
 
   connection {
     host        = self.ipv4_address
@@ -33,23 +36,23 @@ resource "digitalocean_droplet" "web" {
 
   provisioner "remote-exec" {
     inline = [
-      "export PATH=$PATH:/usr/bin",
-      # 1. Install Docker
-      "apt update",
-      "apt -y install apt-transport-https ca-certificates curl software-properties-common",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-      "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | tee /etc/apt/sources.list.d/docker.list > /dev/null",
-      "apt update",
-      "apt-cache policy docker-ce",
-      "apt -y install docker-ce",
-      # "usermod -aG docker ${USER}",
-      "docker -v",
-      # 2. Install Docker Compose
-      "mkdir -p ~/.docker/cli-plugins/",
-      "curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose",
-      "chmod +x ~/.docker/cli-plugins/docker-compose",
-      "docker compose version",
+      "mkdir -p /tmp/scripts/"
     ]
+  }
+
+  provisioner "file" {
+    source      = local.script_directory
+    destination = "/tmp/scripts/"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "bash /tmp/scripts/server-setup.sh"
+    ]
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
--- tf/modules/droplet/scripts/server-setup.sh
+++ tf/modules/droplet/scripts/server-setup.sh
@@ -14,3 +14,23 @@ set -e
 # - Install Docker and Docker-Compose (apt-get install ...)
 # - Launch Docker-Compose with Nginx
 # - Test connection with Nginx from local device
+
+# 1. Install Docker
+apt update
+apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt-cache policy docker-ce
+apt -y install docker-ce
+# "usermod -aG docker ${USER}
+docker -v
+
+# 2. Install Docker Compose
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
+
+# 3. Create Application Deployment Folder
+mkdir -p /var/www/apps
